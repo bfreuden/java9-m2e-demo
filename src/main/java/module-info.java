69626e6a8f78cd3600fabb73ec0bf66092b92902
bfreuden <bruno.freudensprung@orange.fr>
2018-01-09 11:13:25 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module helloworld.java9demo {
+
+    exports helloworld;
+
+}
---
+++ src/test/java/helloworld/HelloAppTest.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+import org.junit.Test;
+
+public class HelloAppTest {
+
+		@Test
+		public void test() {
+			HelloApp.main(new String[0]);
+		}
+	
+}
